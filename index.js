f9b9fa00fbc8ff8f3503662ea670ae97883fe4fd
SleepyBe4r <109447188+SleepyBe4r@users.noreply.github.com>
2024-11-25 22:31:03 -0300

    ultima modificação

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import session from "express-session";
 // importando cookie;
 import cookieParser from "cookie-parser";
 
+import path from "path";
+
 const app = express();
 
 //configurando a session;
@@ -26,7 +28,7 @@ app.use(cookieParser());
 //você pode escolher entre duas bibliotecas: QS ou QueryString
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("pages/public"));
+app.use(express.static(path.join(process.cwd(), 'pages/public')));
 
 const host = "localhost";
 const port = 3000;
